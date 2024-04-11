df1f4854d9b2e2cf780faeaeff46c460dacd055e
Ben Jaros <70767566+bjaros20@users.noreply.github.com>
2024-04-11 10:22:56 -0400

    Update Callaway_Sant_Anna_plotsWorkshop.R
    Removed Plot that didn't work.  Trying to get simple plot for Jonathan

--- Spring24_Workshop/Callaway_Sant_Anna_plotsWorkshop.R
+++ Spring24_Workshop/Callaway_Sant_Anna_plotsWorkshop.R
@@ -304,52 +304,5 @@ ggplot(average_df, aes(x = year)) +
     plot.title = element_text(hjust = 0.5, face = "bold")
   )
 
-# Create the plot
-ggplot(average_df, aes(x = year)) +
-  geom_segment(aes(x = year, xend = year, y = average_CORPINCTX_c - std_err_c, yend = average_CORPINCTX_c + std_err_c, color = "Control Group"), size = 1, linetype = "dashed") +
-  geom_ribbon(aes(ymin = average_CORPINCTX_c - std_err_c, 
-                  ymax = average_CORPINCTX_c + std_err_c, 
-                  fill = "Control Group"), alpha = 0.3) +
-  geom_line(aes(y = average_CORPINCTX_t, color = "Treatment Group"), size = 1) +
-  geom_ribbon(aes(ymin = average_CORPINCTX_t - std_err_t, 
-                  ymax = average_CORPINCTX_t + std_err_t, 
-                  fill = "Treatment Group"), alpha = 0.3) +
-  labs(title = "Average Corporate Income Tax by Year for Control and Treatment Groups",
-       x = "Year",
-       y = "Average Corporate Income Tax in Millions") +
-  scale_color_manual(values = c("Control Group" = "black", "Treatment Group" = "red"),
-                     labels = c("Control Group" = "Control", "Treatment Group" = "Treatment"),
-                     guide = guide_legend(override.aes = list(linetype = c("dashed", "solid")))) +
-  scale_fill_manual(values = c("Control Group" = "lightgray", "Treatment Group" = "lightpink"), guide = FALSE) +
-  guides(color = guide_legend(title = "Groups")) +
-  theme_minimal() +
-  theme(
-    plot.title = element_text(hjust = 0.5, face = "bold")
-  )
 
-# Create the plot
-ggplot(average_df, aes(x = year)) +
-  geom_line(aes(y = average_CORPINCTX_c, color = "Control Group"), size = 1) +
-  geom_ribbon(aes(ymin = average_CORPINCTX_c - std_err_c, 
-                  ymax = average_CORPINCTX_c + std_err_c, 
-                  fill = "Control Group"), alpha = 0.3) +
-  geom_line(aes(y = average_CORPINCTX_t, color = "Treatment Group"), size = 1) +
-  geom_ribbon(aes(ymin = average_CORPINCTX_t - std_err_t, 
-                  ymax = average_CORPINCTX_t + std_err_t, 
-                  fill = "Treatment Group"), alpha = 0.3) +
-  labs(title = "Average Corporate Income Tax by Year for Control and Treatment Groups",
-       x = "Year",
-       y = "Average Corporate Income Tax in Millions") +
-  scale_color_manual(values = c("Control Group" = "black", "Treatment Group" = "red"),
-                     labels = c("Control Group" = "Control", "Treatment Group" = "Treatment")) +
-  scale_fill_manual(values = c("Control Group" = "lightgray", "Treatment Group" = "lightpink"), guide = FALSE) +
-  guides(color = guide_legend(title = "Groups")) +
-  theme_minimal() +
-  theme(
-    plot.title = element_text(hjust = 0.5, face = "bold"),
-    legend.key = element_rect(fill = "white"),
-    legend.linetype = c("solid", "dashed"),
-    legend.key.size = unit(1, "cm")
-  )
-
-
+#Plots Jonathan Wants- Wants the Purest Control Group
