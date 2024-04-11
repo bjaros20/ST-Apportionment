07b9faa65e5bf01f4cef5d47493d46fe0385e886
Ben Jaros <70767566+bjaros20@users.noreply.github.com>
2024-04-11 12:47:06 -0400

    Updates to running DiD
    Created the Log difference plot

---
+++ Rates_Of_Change/RateOfChange_ElasticityTaxBase.R
@@ -0,0 +1,15 @@
+# Get rate of change for tax bases and find elasticity of tax base w.r.t apportionment and rate changes.
+
+#Redo Callaway and Sant'Anna From Summer
+library(readxl)
+library(tidyr)
+library(dplyr)
+library(did)
+library(tidyverse)
+library(lmtest)
+library(caret)
+library(ggplot2)
+
+#New Directory
+setwd("~/Documents/GitHub/ST-Apportionment/Rates_Of_Change")
+
