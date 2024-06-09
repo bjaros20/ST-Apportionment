789917d03b315683b455d7c5f6f8b0fda7871186
Ben Jaros <70767566+bjaros20@users.noreply.github.com>
2024-06-09 18:48:27 -0400

    Completed Rate Merge
    The elasticity_rates_jmp.csv incorporates the cleaned rates.  I also included a csv that has just the cleaned rates, in case that is needed for the future.  Now, just to run a log linear regression.

--- job_market_data/Elasticity_Merge_jmp_data.R
+++ job_market_data/Elasticity_Merge_jmp_data.R
@@ -77,3 +77,59 @@ nospace_rate <- as.data.frame(
 
 #it ran, so will try again.
 str(nospace_rate)
+
+
+
+#Will need to first remove the leading space from the columns:
+colnames(rates) <- str_trim(colnames(rates))
+
+cols_to_modify <- colnames(rates)[grepl("^(1976|1977|1978|1979|1980|1981|1982|1983|1984|1985|1986|1987|1988|1989|1990|1991|1992|1993|1994|1995|1996|1997|1998|1999|2000|2001|2002)$", colnames(rates))]
+
+#Try again but will try to remove the first space from the character columns:
+rates_rem_space <- rates %>%
+  mutate(across(all_of(cols_to_modify), ~ str_remove(., "^ ")))
+
+#didn't work, try again, by forcing the numeric conversion
+rates_rem_space <- rates %>%
+  mutate(across(.cols = -State, .fns = ~ as.numeric(trimws(.))))
+
+rates_rem_space <- rates %>%
+  mutate(across(.cols = -State, .fns = ~ parse_number(trimws(.))))
+
+# Check the structure of the cleaned data
+glimpse(rates_rem_space)
+
+class(rates_rem_space)
+str(rates_rem_space)
+
+#One last thing, make sure all columns titles year is numeric
+
+
+#save this clean rates dataframe
+write.csv(rates_rem_space,"clean_rates_1976-2022.csv")
+
+#this worked, all of the columns in rates_rem_space are numeric.
+rates_cleaned <- rates_rem_space %>%
+  mutate(across(.cols = `1976`:`2002`, .fns = ~ as.numeric(trimws(.))))
+
+# Check the structure of the cleaned data to confirm conversion
+glimpse(rates_cleaned)
+
+#save the one with the columns double checked
+write.csv(rates_cleaned,"rates_jmp.csv")
+
+#Now to merge the rates to the elasticity df
+
+#transform the rates dataframe from a wide to long dataframe
+# Transform rates_cleaned to long format
+rates_long <- rates_cleaned %>%
+  pivot_longer(cols = `1976`:`2023`, names_to = "year", values_to = "rate") %>%
+  mutate(year = as.integer(year))  # Convert year to integer for joining
+
+#use an inner join to merge Elas and rates_long
+Elas2 <- Elas %>%
+  inner_join(rates_long,by=c("state"="State","year"="year"))%>%
+  rename(rates=rate)
+
+#save as Elasticity and Rate Data for JMP
+write.csv(Elas2,"elasticity_rates_jmp.csv")
