f05f3d939ec022d8485bf533de7789888af8454b
Ben Jaros <70767566+bjaros20@users.noreply.github.com>
2024-06-14 15:48:51 -0400

    Prelim Result
    Well, I got a result from the effect of the policy.  The log result is translated.
    
    I need to break that result up by state.    Still working on it.  All day, I have been using from the "Great Plots, Code Summary Stats for Public Workshop" commit.  If I want to include plots in what I send to Dougan, I will likely use it as a guide.  I think either way, I will be sending that estimation by state tonight.  I will include a plot tomorrow.
    
    so to recap: (i) break out that effect, that coefficient from post by state.  Or just send the aggregate regression, when you really understand what that coefficient means.
    (ii) create a plot or two if time.  Due at midnight.

---
+++ job_market_data/Log_Total_Rev_FE_result.R
@@ -0,0 +1,134 @@
+#dougan just wants an estimation of the total revenue
+#What approach should be used?
+
+#Load Packages
+library(tidyr)
+library(dplyr)
+library(tidyverse)
+library(lmtest)
+library(ggplot2)
+library(readxl) #load ssfa switch date
+library(did) # for running DiD
+library(caret) 
+library(plm) # for Two way FE
+library(fixest) # multiple FE
+
+
+#JMP Directory
+setwd("~/Documents/GitHub/ST-Apportionment/job_market_data")
+
+#Load files
+Rev <- read.csv("filled_data_jmp.csv")
+
+Log_rev <- read.csv("filled_log_data_jmp.csv")
+
+Date <- read_xlsx("ssfa_data_jmp.xlsx")
+
+#remove the X column, must be something that happens when saving CSV
+Rev <-Rev %>%
+  select(-X)
+Log_rev <- Log_rev %>%
+  select(-X)
+
+#Now what approach?
+#Going for Did
+#reduce size of date dataframe:
+Date2 <- Date %>%
+  select(state,year_effective,group)
+
+#Merge those dates with Log_rev
+Merge <- full_join(Log_rev,Date2, by="state")
+
+#start with post and treatment columns
+# Create a Post Column
+merged_df2 <- Merge %>%
+  group_by(state) %>%
+  mutate(Post = ifelse(year_effective > year, 0, 1)) %>%
+  ungroup()
+
+#Create a treatment column from the Post Column and the group column
+merged_df3 <- merged_df2 %>%
+  mutate(treatment = ifelse(Post == 1 & group == "t", 1, 0))
+
+#Create a Relative Year column for the Event Study Plot
+# Create rel_year column
+merged_df3$rel_year <- merged_df3$year - merged_df3$year_effective
+
+
+#Create inf column, like Butts for control group. (NEED CONTROL GROUP, NOT CHANGED YET)
+#merged_df3$rel_year[grepl("c", merged_df3$group)] <- "inf"
+
+#Need the result with Total Tax
+Res <- merged_df3 %>%
+  mutate(log_totrev=log(TOTLTAX))
+
+#Replace other NA's in post to be Zero
+Res[c("Post")][is.na(Res[c("Post")])] <-0
+
+#estimate a result for Total Taxes
+treat_var <- "year_effective"
+time_var <- "year"
+
+#Make state a numeric factor
+Res$state <- as.numeric(factor(Res$state))
+
+#Run Callaway and Sant Anna, ran the Callaway and Sant Anna package, and dropped 419 rows 
+#it also gave a warning on small group sizes (all are small).  So, going to try, 
+#just Fixed effects regression
+Res_attgt <- att_gt(yname = "log_totrev",
+                    tname = "year",
+                    idname = "state",
+                    gname = "year_effective",
+                    data = Res)
+
+# Run Two way fixed Effect regression, OLS; dropped almost all abservations, will try again
+log_tot_ols = feols(log_totrev ~ log(Post), Res)
+summary(log_tot_ols)
+
+#going to try plm approach -  https://stackoverflow.com/questions/61633179/model-with-three-fixed-effects-in-plm-package-in-r
+
+#plm FE model
+zz1 <- plm(log_totrev ~ factor(Post),Res)
+
+#simple reg
+Reg <- lm(log_totrev ~ Post + year + state,Res)
+summary(Reg)
+
+#Simple reg with rate
+Reg_rate <- lm(log_totrev ~ Post + year + state + rates, Res)
+summary(Reg_rate)
+#Coefficients:
+#Estimate Std. Error t value Pr(>|t|)    
+#(Intercept) -80.075922   3.457566 -23.160  < 2e-16 ***
+#  Post          0.293586   0.059633   4.923 9.18e-07 ***
+ # year          0.047920   0.001733  27.658  < 2e-16 ***
+#  state        -0.006251   0.001487  -4.204 2.73e-05 ***
+#  rates        -0.016319   0.010532  -1.549    0.121    
+
+#simple reg, with log rate
+reg_lrate <-lm(log_totrev ~ Post + year + state + log(rates), Res)
+#log rate won't run
+
+#Simple reg with rate, ran this and positive result for post went away
+factor_rate <- lm(log_totrev ~ Post + factor(year) + factor(state) + rates, Res)
+summary(factor_rate)
+#result Post              0.015200   0.013753   1.105 0.269179 
+
+
+#try sales as numeric variable
+sales_reg <- lm(log_totrev ~ sales + factor (year)+ factor(state) + rates, Res)
+summary(sales_reg)
+#after running the above regression, got the following result for sales
+# sales             0.0006130  0.0002465   2.487 0.012958 *  
+# calculate the transform. .000613 is the log revenue in thousands, it is the B
+# e^B -1 = percentage change from increasing sales factor
+#e^.000613 - 1 = .000613 * 100= 0.0613 %, but that increase on billions is significant
+# this is on the weight of sales factor, so is multiplied by that scalar
+
+
+#For comparison, will try with log sales, #log rate will not run
+log_sales_reg <- lm(log_totrev ~ log(sales) + factor (year)+ factor(state) + rates, Res)
+summary(log_sales_reg)
+# result for log(sales)        0.05897    0.01525   3.868 0.000113 ***
+
+
