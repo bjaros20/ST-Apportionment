9c493d6ed6c2d51cf4530fef2f1ba786195b8724
Ben Jaros <70767566+bjaros20@users.noreply.github.com>
2024-06-17 20:40:13 -0400

    CPI Deflator
    Added a CPI deflator from the BLS.  It is saved as an xlsx. file.  The code so far, merge the CPI to the current revenue panel.   I then mutated and created a deflated total revenue csv.

--- job_market_data/Real_Total_Rev_coef_estimates.R
+++ job_market_data/Real_Total_Rev_coef_estimates.R
@@ -8,9 +8,45 @@ library(dplyr)
 library(tidyverse)
 library(lmtest)
 library(ggplot2)
+install.packages("readxl")
 library(readxl) #load ssfa switch date
 library(did) # for running DiD
 library(caret) 
 library(plm) # for Two way FE
 library(fixest) # multiple FE
 library(broom) #extract coefficients for each state
+
+# set seed
+set.seed(26)
+
+#JMP Directory
+setwd("~/Documents/GitHub/ST-Apportionment/job_market_data")
+
+#Load Data, Coefficient Plot & Rev Data for estimates
+Coef <-read.csv("state_post_dummy_total_rev._6_15_24.csv")
+
+Rev <-read.csv("revenue_panel_total_rev_state_coef_result_6_15_24.csv")
+
+CPI <- read_excel("/Users/ben/Documents/GitHub/ST-Apportionment/job_market_data/cpi.xlsx")
+
+#Remove X
+#remove the X column, must be something that happens when saving CSV
+Rev <-Rev %>%
+  select(-X)
+
+#Merge CPI and Rev
+Rev1 <- Rev %>%
+  full_join(CPI,by=c("year"="Year"))
+
+#save Revenue with CPI
+write.csv(Rev1,"revenues_cpi_6_17_24.csv")
+
+#Just create real revenue for Total Revenue
+Rev2 <- Rev1 %>%
+mutate(real_totRev = TOTLTAX/Annual)
+
+#rename annual column to CPI
+Rev3 <- Rev2 %>%
+  rename(CPI_def=Annual)
+
+write.csv(Rev3,"revenue_cpi_total.csv")
