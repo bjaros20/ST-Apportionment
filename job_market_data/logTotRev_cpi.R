848b71fa534a4559fb0adb0c7716fa58a01d3ff5
Ben Jaros <70767566+bjaros20@users.noreply.github.com>
2024-06-23 22:36:25 -0400

    Real Rev and CIt per capita
    Simple DiD estimates per capita for Real total revenue and corporate income.  Associated plots.

--- job_market_data/logTotRev_cpi.R
+++ job_market_data/logTotRev_cpi.R
@@ -91,8 +91,7 @@ real_perCapita <- per_capita %>%
 ggplot(real_perCapita2, aes(x=year,y=real_totRev_capita_conv, color=State_Acronym))+
   geom_point()
 
-
-#Estimate the simple DiD with the 2007 switchers
+# PART I -Estimate the simple DiD with the 2007 switchers
 #Anyone before needs to be filtered out (a, i)
 #anyone who switches within 3 years 2008-2010, needs to be filtered out (a, ii)
 #leaves treatment (a,iii) 2007 Switchers and control anyone who switches after 2010 (a, iv)
@@ -154,7 +153,7 @@ did_estimate
 #Attempt to create plot
 # Calculate average Log Total Tax by year for control group along with standard error
 average_c <- control_states %>%
- group_by(year) %>%
+  group_by(year) %>%
   filter(!is.na(logRealTotRev)) %>%
   summarise(average_TOTLTX_c = mean(logRealTotRev),
             std_err_c = sd(logRealTotRev) / sqrt(n()))
@@ -201,19 +200,195 @@ ggplot(average_df, aes(x = year)) +
   xlim(2004, 2010) +
   theme_minimal()
 
-# Attempt to make the standard DiD plot
-agg.esNYTLog <- aggte(Log_C_NYT_attgt, type = "dynamic")
-summary(agg.esNYTLog)
 
-ggdid_plotNYTLog <- ggdid(agg.esNYTLog) +
-  scale_x_continuous(limits = c(-10, 10)) +
-  ggtitle("Event Study Plot (Dynamic ATT): Log Corporate Income Tax Revenue, Treatment = Control via NYT") +
-  theme(plot.title = element_text(color = "black", face = "bold", hjust = 0.5)) +
-  geom_vline(xintercept = c(0, 1), linetype = "dashed") +
-  ylab("Log Dollars") +  # Label for y-axis
-  xlab("Year in Reference to Treatment")  # Label for x-axis
+#PART II - Estimate the simple DiD with the 2007 switchers for the real revenue per capita.
+#Anyone before needs to be filtered out (a, i)
+#anyone who switches within 3 years 2008-2010, needs to be filtered out (a, ii)
+#leaves treatment (a,iii) 2007 Switchers and control anyone who switches after 2010 (a, iv)
+#(b) https://rpubs.com/phle/r_tutorial_difference_in_differences
 
-print(ggdid_plotNYTLog)
+#Create a dataframe with just 2007 states
+treated_states <- real_perCapita %>%
+  filter(year_effective==2007)
+
+#create df for control group
+control_states <- real_perCapita %>%
+  filter(year_effective >=2011 | is.na(year_effective))
+
+#Create dataframe with states in control_states
+states_1df <- control_states %>%
+  distinct(State_Acronym)
+
+print(states_1df)
+
+states_2df <- treated_states %>%
+  distinct(State_Acronym)
+
+print(states_2df)
+
+# find mean of control_States, pre & post
+control_pre <-control_states %>%
+  filter(year <= 2007) %>%
+  summarize(avg_outcome = mean(real_totRev_capita, na.rm= TRUE))
+
+control_post <-control_states %>%
+  filter(year >2007 & year<2011) %>%
+  summarize(avg_outcome = mean(real_totRev_capita, na.rm= TRUE))
+
+# calculate averages for treatment group
+treatment_pre <- treated_states %>%
+  filter(year <= 2007) %>%
+  summarize(avg_outcome = mean(real_totRev_capita, na.rm = TRUE))
+
+treatment_post <- treated_states %>%
+  filter(year >2007 & year<2011) %>%
+  summarise(avg_outcome = mean(real_totRev_capita, na.rm = TRUE))
+
+# Calculate differences
+control_diff <- control_post$avg_outcome - control_pre$avg_outcome
+treatment_diff <- treatment_post$avg_outcome - treatment_pre$avg_outcome
+
+# Calculate Difference-in-Differences
+did_estimate <- treatment_diff - control_diff
+
+# Print the DiD estimate
+did_estimate
+#[1] -131.2836
 
 
+#Attempt to create plot
+# Calculate averagereal_totRev_capita by year for control group along with standard error
+average_c <- control_states %>%
+  group_by(year) %>%
+  filter(!is.na(real_totRev_capita)) %>%
+  summarise(average_TOTLTX_c = mean(real_totRev_capita),
+            std_err_c = sd(real_totRev_capita) / sqrt(n()))
 
+# Calculate average real_totRev_capita by year for treated states along with standard error
+average_t <- treated_states %>%
+  group_by(year) %>%
+  filter(!is.na(real_totRev_capita)) %>%
+  summarise(average_TOTLTX_t = mean(real_totRev_capita),
+            std_err_t = sd(real_totRev_capita) / sqrt(n()))
+
+# Merge the two dataframes by year
+average_df <- merge(average_c, average_t, by = "year", all = TRUE)
+
+# Create the plot, standard bar plot with standard error bars
+ggplot(average_df, aes(x = year)) +
+  geom_point(aes(y = average_TOTLTX_c, color = "Control Group"), size = 3) +
+  geom_errorbar(aes(y = average_TOTLTX_c, ymin = average_TOTLTX_c - std_err_c, ymax = average_TOTLTX_c + std_err_c, color = "Control Group"), width = 0.2) +
+  geom_point(aes(y = average_TOTLTX_t, color = "Treatment Group"), size = 3) +
+  geom_errorbar(aes(y = average_TOTLTX_t, ymin = average_TOTLTX_t - std_err_t, ymax = average_TOTLTX_t + std_err_t, color = "Treatment Group"), width = 0.2) +
+  geom_vline(xintercept = 2007, linetype = "dashed", color = "black", size = 1) +
+  labs(title = "Average Real Total Tax Revenue per Capita by Year for Control and Treatment Groups",
+       x = "Year",
+       y = "Average Real Total Tax Revenue per Capita") +
+  scale_color_manual(values = c("Control Group" = "blue", "Treatment Group" = "red"),
+                     labels = c("Control Group" = "Group c", "Treatment Group" = "Group t")) +
+  guides(color = guide_legend(title = "Groups")) +
+  theme_minimal()
+
+
+# Now modify the plot to only have years 2004-2010
+ggplot(average_df, aes(x = year)) +
+  geom_point(aes(y = average_TOTLTX_c, color = "Control Group"), size = 3) +
+  geom_errorbar(aes(y = average_TOTLTX_c, ymin = average_TOTLTX_c - std_err_c, ymax = average_TOTLTX_c + std_err_c, color = "Control Group"), width = 0.2) +
+  geom_point(aes(y = average_TOTLTX_t, color = "Treatment Group"), size = 3) +
+  geom_errorbar(aes(y = average_TOTLTX_t, ymin = average_TOTLTX_t - std_err_t, ymax = average_TOTLTX_t + std_err_t, color = "Treatment Group"), width = 0.2) +
+  geom_vline(xintercept = 2007, linetype = "dashed", color = "black", size = 1) +
+  labs(title = "Average Real Total Tax Revenue per Capita by Year for Control and Treatment Groups",
+       x = "Year",
+       y = "Average Real Total Tax Revenue per Capita") +
+  scale_color_manual(values = c("Control Group" = "blue", "Treatment Group" = "red"),
+                     labels = c("Control Group" = "Group c", "Treatment Group" = "Group t")) +
+  guides(color = guide_legend(title = "Groups")) +
+  xlim(2004, 2010) +
+  theme_minimal()
+
+
+# PART III Corporate Income Per Capita
+#Don't need to recreate dfs
+
+# find mean of control_States, pre & post
+control_pre <-control_states %>%
+  filter(year <= 2007) %>%
+  summarize(avg_outcome = mean(real_cit_capita, na.rm= TRUE))
+
+control_post <-control_states %>%
+  filter(year >2007 & year<2011) %>%
+  summarize(avg_outcome = mean(real_cit_capita, na.rm= TRUE))
+
+# calculate averages for treatment group
+treatment_pre <- treated_states %>%
+  filter(year <= 2007) %>%
+  summarize(avg_outcome = mean(real_cit_capita, na.rm = TRUE))
+
+treatment_post <- treated_states %>%
+  filter(year >2007 & year<2011) %>%
+  summarise(avg_outcome = mean(real_cit_capita, na.rm = TRUE))
+
+# Calculate differences
+control_diff <- control_post$avg_outcome - control_pre$avg_outcome
+treatment_diff <- treatment_post$avg_outcome - treatment_pre$avg_outcome
+
+# Calculate Difference-in-Differences
+did_estimate <- treatment_diff - control_diff
+
+# Print the DiD estimate, CIT per capita estimate
+did_estimate
+#[1] -14.29405
+
+
+#Attempt to create plot
+# Calculate real_cit_capita by year for control group along with standard error
+average_c <- control_states %>%
+  group_by(year) %>%
+  filter(!is.na(real_cit_capita)) %>%
+  summarise(average_TOTLTX_c = mean(real_cit_capita),
+            std_err_c = sd(real_cit_capita) / sqrt(n()))
+
+# Calculate average real_cit_capita by year for treated states along with standard error
+average_t <- treated_states %>%
+  group_by(year) %>%
+  filter(!is.na(real_cit_capita)) %>%
+  summarise(average_TOTLTX_t = mean(real_cit_capita),
+            std_err_t = sd(real_cit_capita) / sqrt(n()))
+
+# Merge the two dataframes by year
+average_df <- merge(average_c, average_t, by = "year", all = TRUE)
+
+# Create the plot, standard bar plot with standard error bars
+ggplot(average_df, aes(x = year)) +
+  geom_point(aes(y = average_TOTLTX_c, color = "Control Group"), size = 3) +
+  geom_errorbar(aes(y = average_TOTLTX_c, ymin = average_TOTLTX_c - std_err_c, ymax = average_TOTLTX_c + std_err_c, color = "Control Group"), width = 0.2) +
+  geom_point(aes(y = average_TOTLTX_t, color = "Treatment Group"), size = 3) +
+  geom_errorbar(aes(y = average_TOTLTX_t, ymin = average_TOTLTX_t - std_err_t, ymax = average_TOTLTX_t + std_err_t, color = "Treatment Group"), width = 0.2) +
+  geom_vline(xintercept = 2007, linetype = "dashed", color = "black", size = 1) +
+  labs(title = "Average Real Corporate Tax Revenue per Capita by Year for Control and Treatment Groups",
+       x = "Year",
+       y = "Average Real Corporate Revenue per Capita") +
+  scale_color_manual(values = c("Control Group" = "blue", "Treatment Group" = "red"),
+                     labels = c("Control Group" = "Group c", "Treatment Group" = "Group t")) +
+  guides(color = guide_legend(title = "Groups")) +
+  theme_minimal()
+
+
+# Now modify the plot to only have years 2004-2010
+ggplot(average_df, aes(x = year)) +
+  geom_point(aes(y = average_TOTLTX_c, color = "Control Group"), size = 3) +
+  geom_errorbar(aes(y = average_TOTLTX_c, ymin = average_TOTLTX_c - std_err_c, ymax = average_TOTLTX_c + std_err_c, color = "Control Group"), width = 0.2) +
+  geom_point(aes(y = average_TOTLTX_t, color = "Treatment Group"), size = 3) +
+  geom_errorbar(aes(y = average_TOTLTX_t, ymin = average_TOTLTX_t - std_err_t, ymax = average_TOTLTX_t + std_err_t, color = "Treatment Group"), width = 0.2) +
+  geom_vline(xintercept = 2007, linetype = "dashed", color = "black", size = 1) +
+  labs(title = "Average Real CIT Tax Revenue per Capita by Year for Control and Treatment Groups",
+       x = "Year",
+       y = "Average Real CIT  Tax Revenue per Capita") +
+  scale_color_manual(values = c("Control Group" = "blue", "Treatment Group" = "red"),
+                     labels = c("Control Group" = "Group c", "Treatment Group" = "Group t")) +
+  guides(color = guide_legend(title = "Groups")) +
+  xlim(2004, 2010) +
+  theme_minimal()
+
+#Save the dataframe for per_capita
+write.csv(real_perCapita,"real_per_capita_df.csv")
