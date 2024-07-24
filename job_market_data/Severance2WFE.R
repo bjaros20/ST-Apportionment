d2d8fdfea499228c7463c2bc04035d1af7443da5
Ben Jaros <70767566+bjaros20@users.noreply.github.com>
2024-07-23 22:44:03 -0400

    Severance Result
    Severance Result. In process, created original FRED Rev merge.

--- job_market_data/Severance2WFE.R
+++ job_market_data/Severance2WFE.R
@@ -25,14 +25,17 @@ Rev <- read.csv("filled_data_jmp.csv")
 NoCIT <- read.csv("Non_CIT_states_FRED_OH.csv")
 
 
-
 #Merge NoCIT with Rev2, then select Relevant variables
 Merge2 <- NoCIT %>%
   bind_rows(Rev)
 
+#Now, need to just bet back to the original FRED Revenues alone
+Orig <- Merge2 %>%
+  select(-X,-industry_dum,-election_dum,-phase_in_dum,-rates,-Number)
+
+#Merge Rates and Switch Data onto Original data
+
 SSFA <- read_xlsx("ssfa_data_jmp.xlsx")
-
-
 
 #Load rates dataframe
 Rates<- read.csv("clean_rates_1976-2022.csv")
@@ -45,5 +48,51 @@ Rates2 <- read.csv("rates_jmp.csv")
 #dataset is that that Rev alone contains all states
 # Even Rev doesn't contain Texas.  Will need to go earlier.
 
+#Orig now needs rate merged... actually will pause on that because rate is CIT rate
+
+Sev <- Orig %>%
+  select(State_Acronym,year,SVRNCTAX,state,sales,Region)
+
+#Create df of State Acronyms and State Names
+State_Acronym <- c("AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DE", "FL", "GA", 
+                   "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", 
+                   "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", 
+                   "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", 
+                   "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY")
+
+state <- c("Alaska", "Alabama", "Arkansas", "Arizona", "California", "Colorado", 
+                "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Iowa", 
+                "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", 
+                "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", 
+                "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", 
+                "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", 
+                "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", 
+                "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", 
+                "Virginia", "Vermont", "Washington", "Wisconsin", "West Virginia", 
+                "Wyoming")
+
+state_data <- data.frame(State_Acronym, state)
+print(state_data)
+
+#Merge with Orig
+Orig2 <- Orig %>%
+  left_join(state_data,by=c("State_Acronym"))
+
+Orig3 <- Orig2 %>%
+  select(-state.x)%>%
+  rename(state=state.y)
+
+write.csv(Orig3,"FRED_rev_all_states.csv")
+
+#Severance Data
+Sev <- Orig3 %>%
+  select(State_Acronym,year,SVRNCTAX,state,sales)
+
+#Merge Sev with SSFA2
+SSFA2 <- SSFA %>%
+  select(state,year_effective)
+
+Sev2 <- Sev %>%
+  full_join(SSFA2, by= c("state"))
 
 
