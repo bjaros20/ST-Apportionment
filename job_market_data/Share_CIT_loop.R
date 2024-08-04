aaccd0c7aeb8f4b15575918f2e90a11a214037b3
Ben Jaros <70767566+bjaros20@users.noreply.github.com>
2024-08-04 17:41:18 -0400

    updates to CIT loop
    Here this is.
    
    Going on Flight.
    
    Ben

--- job_market_data/Share_CIT_loop.R
+++ job_market_data/Share_CIT_loop.R
@@ -1,3 +1,30 @@
 #Will use the loop created from loop_sDiD, will modify it to get outputs ordered
 # in better fashion.
 
+#Load Packages
+library(tidyr)
+library(dplyr)
+library(tidyverse)
+library(lmtest)
+library(ggplot2)
+library(did) # for running DiD
+library(plm)
+library(lmtest)
+library(synthdid)
+library(fixest)
+library(boot)
+library(ggthemes)
+
+# set seed
+set.seed(26)
+#JMP Directory
+setwd("~/Documents/GitHub/ST-Apportionment/job_market_data")
+
+#Load Detrended, per capita, real revenue data
+Rev <- read.csv("detrend_per_capita.csv")
+
+
+Corp <-read.csv("CIT_sDID_loop.csv")
+
+
+
