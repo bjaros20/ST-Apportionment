9df247fbad708c817a8ffdae995589862b66d07f
Ben Jaros <70767566+bjaros20@users.noreply.github.com>
2024-09-21 20:43:25 -0400

    Synthetic Control Attempt
    The R script, images, and history for attempting Synthetic Control. I need more information in order to get a good synthetic control created.
    
    I need to integrate the time weights appropriately, and extend them out over the treated years.

--- job_market_data/loop_DF_perc_Change.R
+++ job_market_data/loop_DF_perc_Change.R
@@ -31,10 +31,13 @@ setwd("~/Documents/GitHub/ST-Apportionment/job_market_data")
 naive_ci<-read.csv("naive_ci.csv")
 real_CI <- naive_ci%>%
   mutate(real_ci = (naive_ci/CPI_def)*100)
+real_CI_cap <- real_CI %>%
+  mutate(real_ci_cap = real_ci/population)
+
 
 #Create base dataframe that has nat_share as dependent variable.
-Filter_frac <-real_CI %>%
-  select(State_Acronym,year,year_effective,State_Name,real_ci,Post)
+Filter_frac <-real_CI_cap %>%
+  select(State_Acronym,year,year_effective,State_Name,real_ci_cap,Post,logRealCitRev,population)
 
 not_yet_treated <- Filter_frac %>%
   group_by(State_Name)%>%
@@ -48,9 +51,9 @@ never_treated <- Filter_frac %>%
 control <- rbind(never_treated,not_yet_treated) %>%
   distinct(State_Name)
 
-control_noAK_HA <-rbind(never_treated,not_yet_treated)%>%
-  filter(!(State_Name == "Alaska" | State_Name == "Hawaii")) %>%
-  distinct(State_Name)
+#control_noAK_HA <-rbind(never_treated,not_yet_treated)%>%
+#  filter(!(State_Name == "Alaska" | State_Name == "Hawaii")) %>%
+#  distinct(State_Name)
 
 
 #Create Treatment dataframe for just Illinois
@@ -60,11 +63,28 @@ Illinois <- Filter_frac %>%
 
 # synthetic control
 illinois_sc <- Illinois %>%
-  synthetic_control(outcome = real_ci,
+  synthetic_control(outcome = real_ci_cap,
                     unit = State_Name,
+                    time= year,
                     i_unit = "Illinois",
                     i_time = 1999,
-                    generate_placebos = TRUE)
+                    generate_placebos = TRUE) %>%
+  # Generate predictors (you can adjust these as needed)
+  generate_predictor(time_window = 1976:1999,
+                     population = mean(population, na.rm = TRUE)) %>%  # required to put one predictor. population
+    # Generate the fitted weights for the synthetic control
+  generate_weights(optimization_window = 1976:1999,  # Time to use in the optimization task
+                 margin_ipop = .02,
+                 sigf_ipop = 7,
+                 bound_ipop = 6  # Optimizer options
+) %>%
+  generate_control()
+
+summary(illinois_sc)
 
 
+illinois_sc %>% plot_trends()
 
+illinois_sc %>% plot_differences()
+
+illinois_sc %>% plot_weights()
