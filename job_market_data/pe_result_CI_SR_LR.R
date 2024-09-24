ffc629c85829b4b0c179c015ec7c05866652058b
Ben Jaros <70767566+bjaros20@users.noreply.github.com>
2024-09-23 21:16:48 -0400

    Create pe_result_CI_SR_LR.R

---
+++ job_market_data/pe_result_CI_SR_LR.R
@@ -0,0 +1,5 @@
+#Redo Main result script.
+# main difference here is that I am going to compute the 3 year window explicitly as a short run for CI
+#Then I will create a long run run loop.  I will save the plots for both.
+#save the synthetic units in a dataframe.  Those units will be used to create the normalized shift data
+
