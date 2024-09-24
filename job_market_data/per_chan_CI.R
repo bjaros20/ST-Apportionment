c2db07e143d408aa3c9edbb10ec4dfd0140f1968
Ben Jaros <70767566+bjaros20@users.noreply.github.com>
2024-09-24 13:46:45 -0400

    Created Percentage Change in CI script
    This is the percentage change in corporate income script.  I renamed the older script by that name as a trial for a fresh start.

---
+++ job_market_data/per_chan_CI.R
@@ -0,0 +1,28 @@
+#percent change for corporate income
+#use the the weights created from using the never treated.  
+
+#Load Packages
+library(tidyr)
+library(dplyr)
+library(tidyverse)
+library(lmtest)
+library(ggplot2)
+library(did) # for running DiD
+library(plm)
+library(lmtest)
+library(synthdid)
+library(fixest)
+library(boot)
+library(ggthemes)
+
+# set seed
+set.seed(26)
+#JMP Directory
+setwd("~/Documents/GitHub/ST-Apportionment/job_market_data")
+
+#Laod data
+naive_ci<-read.csv("naive_ci.csv")
+controls <-read.csv("sDiD_point_estimate_list_CI_long_run.csv")
+
+print(controls)
+
