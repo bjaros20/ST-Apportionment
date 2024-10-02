9c9431c9b11880ce5be4f1cd545e2d9ffe24fb77
Ben Jaros <70767566+bjaros20@users.noreply.github.com>
2024-10-02 01:26:26 -0400

    Create per_chan_NCI.R

---
+++ job_market_data/per_chan_NCI.R
@@ -0,0 +1,19 @@
+# Percentage Change in Non-corporate income
+#Load Packages
+library(tidyr)
+library(dplyr)
+library(tidyverse)
+library(lmtest)
+library(ggplot2)
+library(did) # for running DiD
+library(plm)
+library(lmtest)
+library(synthdid)
+library(fixest)
+library(boot)
+library(ggthemes)
+
+# set seed
+set.seed(26)
+#JMP Directory
+setwd("~/Documents/GitHub/ST-Apportionment/job_market_data")
